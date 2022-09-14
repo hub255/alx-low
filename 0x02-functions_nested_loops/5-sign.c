3cd66b5d28487ca75eb76ea9fd37d86a1404f6fa
bob otieno <otienoagd@gmail.com>
2022-09-14 02:46:37 -0700

    signs only

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,8 @@
 *print_sign-print sign
 *
 *@n: character to comparewith
-*return: 1 if positive, 0 if zero, -1 if negative
+*
+*Return: 1 if positive, 0 if zero, -1 if negative
 */
 int print_sign(int n)
 {
