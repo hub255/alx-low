a6d59f540a8dd0efd3ffb14a90a4c1a574b3b14f
bob otieno <otienoagd@gmail.com>
2022-09-14 05:55:12 -0700

    numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,31 +1,36 @@
 #include "main.h"
 #include "6-abs.c"
 #include <stdio.h>
-void print_to_98(int j) 
+/**
+*print_to_98 - print numbers
+*
+*@j: compare with j
+*Return: number followed by space
+*/
+void print_to_98(int j)
 {
 int i;
 if (j > 98)
-{      
-for (i = j; i >= 98; i--)	
-{  
-printf("%d", i);	  
-if (i != 98)	    
-{      
-printf(", ");	      
-}  
-}     
+{
+for (i = j; i >= 98; i--)
+{
+printf("%d", i);
+if (i != 98)
+{
+printf(", ");
 }
-else   
-{      
+}
+}
+else
+{
 for (i = j; i <= 98; i++)
-{	  
-printf("%d", i);	  
-if (i != 98)	    
-{	      
-printf(", ");	      
-}	  
-}      
-}  
- printf("\n"); 
+{
+printf("%d", i);
+if (i != 98)
+{
+printf(", ");
 }
-
+}
+}
+ printf("\n");
+}
