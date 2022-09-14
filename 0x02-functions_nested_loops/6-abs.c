2336f22eb7a24ce9ec36f00c848d4f58e5b8afb7
bob otieno <otienoagd@gmail.com>
2022-09-14 02:54:30 -0700

    abs of  number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_abs- prints absolute number
+*
+*@n: character to use
+*Return: a positive integer
+*/
+int _abs(int i)
+{
+if (i > 0)
+{
+return (i);
+}
+else
+{
+return (i * -1);
+}
